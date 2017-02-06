cd59b7185455291018ee54b387449f29d233cd59
jakkodejong <jakko.pieter.de.jong@qnh.nl>
2017-02-06 17:26:24 +0100

    Update cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that stores a matrix 'x' as a global variable in the global namespace. 
+## Furthermore, it creates a global variable 'inv', with initial value NULL, to store the inverse of matrix x in the global namespace.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv_matrix) inv <<- inv_matrix
+        getInv <- function() inv
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the function list created by makeCacheMatrix, gets the stored matrix, and finds out if an inverse matrix was already cached.
+## If so, it returns this cached inverse matrix. If not, it calculates the inverse and stores the result by calling x$setInv().                
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)){
+                message("getting cached inverse of matrix x.")
+                return(inv)
+        }
+        message("calculating inverse of matrix x.")
+        data <- x$get()
+        inv <- solve(x$get())
+        x$setInv(inv)
+        inv
 }
